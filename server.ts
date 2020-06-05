7bb362ec4c2a5a8ab8f33aaa775b6b015fcb0091
dartho2 <wegrzyn_pawel@outlook.com>
2020-06-05 23:18:05 +0200

    mm1

--- server.ts
+++ server.ts
@@ -1,27 +1,12 @@
-import 'zone.js/dist/zone-node';
-import 'reflect-metadata';
-import {enableProdMode} from '@angular/core';
-// Express Engine
-import {ngExpressEngine} from '@nguniversal/express-engine';
-// Import module map for lazy loading
-import {provideModuleMap} from '@nguniversal/module-map-ngfactory-loader';
-
-import * as express from 'express';
-import {join} from 'path';
-
-// Faster server renders w/ Prod mode (dev mode never needed)
-enableProdMode();
-
-// Express server
-// const app = express();
+// global['window'] = window;
 const express = require('express');
 const path = require('path');
 const app = express();
 const http = require('http');
 const https = require('https');
 const fs = require('fs');
-const privateKey = fs.readFileSync('./cert/2BABF81C34AB39706EA004FE027FF72F.key');
-const certyficate = fs.readFileSync('./cert/2BABF81C34AB39706EA004FE027FF72F.crt');
+const privateKey = fs.readFileSync('./cert/key.key');
+const certyficate = fs.readFileSync('./cert/crt.crt');
 const credentials = {
   key: privateKey,
   cert: certyficate
@@ -33,42 +18,16 @@ app.use(function(req, res, next) {
       res.redirect('https://' + req.headers.host + req.url);
   }
 });
+
+app.use(express.static('./dist/wooko'));
+
 const httpServer = http.createServer(app);
 const httpsServer = https.createServer(credentials, app);
 
-const PORT = process.env.PORT || 4000;
-const DIST_FOLDER = join(process.cwd(), 'dist/browser');
-
-// * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const {AppServerModuleNgFactory, LAZY_MODULE_MAP} = require('./dist/server/main');
-
-// Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-app.engine('html', ngExpressEngine({
-  bootstrap: AppServerModuleNgFactory,
-  providers: [
-    provideModuleMap(LAZY_MODULE_MAP)
-  ]
-}));
-
-app.set('view engine', 'html');
-app.set('views', DIST_FOLDER);
-
-// Example Express Rest API endpoints
-// app.get('/api/**', (req, res) => { });
-// Server static files from /browser
-app.get('*.*', express.static(DIST_FOLDER, {
-  maxAge: '1y'
-}));
-
-// All regular routes use the Universal engine
-app.get('*', (req, res) => {
-  res.render('index', { req });
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/dist/wooko/index.html'));
 });
 
-// Start up the Node server
-// app.listen(PORT, () => {
-//   console.log(`Node Express server listening on http://localhost:${PORT}`);
-// });
 httpServer.listen(80, () => {
   console.log('Server Https started on 80')
 });
